39afdceeac33e61f568b17d0851697dc0fa07260
alkodrimuhfaris <farisalkodri@gmail.com>
2021-09-26 19:57:49 +0700

    Add start scripts

--- src/app.js
+++ src/app.js
@@ -3,7 +3,7 @@ const express = require('express');
 const morgan = require('morgan');
 const cors = require('cors');
 const response = require('./helpers/response');
-const port = 8080;
+const port = 5000;
 
 const app = express();
 
